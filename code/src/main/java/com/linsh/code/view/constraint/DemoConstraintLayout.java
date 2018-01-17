3699cbe5f794e2d61b14b3a77bcfe18b0249f0bd
SenhLinsh <lyuanxiang0812@126.com>
2018-01-17 14:01:47 +0800

    ConstraintLayout XML属性详解及谷歌示例

---
+++ code/src/main/java/com/linsh/code/view/constraint/DemoConstraintLayout.java
@@ -0,0 +1,18 @@
+package com.linsh.code.view.constraint;
+
+import com.linsh.code.R;
+
+/**
+ * <pre>
+ *    author : Senh Linsh
+ *    github : https://github.com/SenhLinsh
+ *    date   : 2018/01/16
+ *    desc   :
+ * </pre>
+ */
+public class DemoConstraintLayout {
+
+    private int layout = R.layout.constraint_layout;        // ConstraintLayout XML 属性详解
+
+    private int layout2 = R.layout.constraint_layout_google; // 谷歌示例
+}
