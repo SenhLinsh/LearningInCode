8cdd8f7366ceca9c9fbd94791ec0e032e824126a
Senh Linsh <lyuanxiang0812@126.com>
2018-11-22 15:27:47 +0800

    fix: 配置修复

--- code/src/main/java/com/linsh/code/view/constraint/LearnLayingOutConstraintLayout.java
+++ code/src/main/java/com/linsh/code/view/constraint/LearnLayingOutConstraintLayout.java
@@ -1,7 +1,5 @@
 package com.linsh.code.view.constraint;
 
-import com.linsh.code.R;
-
 /**
  * <pre>
  *    author : Senh Linsh
@@ -11,8 +9,72 @@ import com.linsh.code.R;
  * </pre>
  */
 public class LearnLayingOutConstraintLayout {
-
-    private int layout = R.layout.constraint_layout;        // ConstraintLayout XML 属性详解
-
-    private int layout2 = R.layout.constraint_layout_google; // 谷歌示例 https://github.com/googlecodelabs/constraint-layout
 }
+
+// ####### ConstraintLayout XML 属性详解 ########
+/*
+
+<?xml version="1.0" encoding="utf-8"?>
+<android.support.constraint.ConstraintLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+
+    <View
+        android:layout_width="0dp"
+        android:layout_height="40dp"
+
+        // 控制上下左右边的约束
+        app:layout_constraintLeft_toLeftOf="parent"             // 产生约束: 左边和指定控件的左边
+        app:layout_constraintLeft_toRightOf="@id/guideline1"    // 产生约束: 左边和指定控件的右边
+        app:layout_constraintRight_toLeftOf="@id/guideline1"    // 产生约束: 右边和指定控件的左边
+        app:layout_constraintRight_toRightOf="parent"           // 产生约束: 右边和指定控件的右边
+        app:layout_constraintStart_toEndOf="@id/guideline1"     // 产生约束: 开始边和指定控件的结束边
+        app:layout_constraintStart_toStartOf="parent"           // 产生约束: 开始边和指定控件的开始边
+        app:layout_constraintEnd_toEndOf="parent"               // 产生约束: 结束边和指定控件的结束边
+        app:layout_constraintEnd_toStartOf="@id/guideline1"     // 产生约束: 结束边和指定控件的开始边
+        app:layout_constraintTop_toBottomOf="@id/guideline2"    // 产生约束: 顶边和指定控件的底边
+        app:layout_constraintTop_toTopOf="parent"               // 产生约束: 顶边和指定控件的顶边
+        app:layout_constraintBottom_toBottomOf="parent"         // 产生约束: 底边和指定控件的底边
+        app:layout_constraintBottom_toTopOf="@+id/guideline2"   // 产生约束: 底边和指定控件的顶边
+
+        // 控制基线的约束
+        app:layout_constraintBaseline_toBaselineOf="parent"     // 产生约束: 基线和指定控件的基线
+
+        // 控制宽高比
+        app:layout_constraintDimensionRatio="2:1"               // 宽:高 = 2:1 (要求宽高值有一项为0)
+        // 辅助控制高度
+        app:layout_constraintHeight_default="spread"            // 默认高度 [spread:相当于match_parent, wrap:相当于wrap_content]
+        app:layout_constraintHeight_max="50dp"                  // 最大高度
+        app:layout_constraintHeight_min="30dp"                  // 最小高度
+        // 辅助控制宽度
+        app:layout_constraintWidth_default="spread"             // 默认宽度
+        app:layout_constraintWidth_max="50dp"                   // 最大宽度
+        app:layout_constraintWidth_min="30dp"                   // 最小宽度
+        // 控制水平约束
+        app:layout_constraintHorizontal_bias="0.5"              // 水平约束的偏移 (0为左侧, 0.5居中, 1为右侧)
+        app:layout_constraintHorizontal_chainStyle="spread"     // 群组关联样式 [spread:展开,存在多余空间则平分(默认); spread_inside:展开,存在多余空间,两端不占空间,其余平分; packed:包裹,群组两侧产生约束使它们聚集在一起]
+        app:layout_constraintHorizontal_weight="1"              // 群组关联样式为 spread 或 spread_inside 时, 各 View 平分剩余空间的权重
+        // 控制垂直约束
+        app:layout_constraintVertical_bias="0.5"                // 同理于控制水平约束
+        app:layout_constraintVertical_chainStyle="spread"       // 同理于控制水平约束
+        app:layout_constraintVertical_weight="1"/>              // 同理于控制水平约束
+
+        // 群组: View 之间互相约束, 会产生相互影响, 组成一个"群组".
+        // 群组中的第一个 View 被称之为 Chain head, 由它控制着整个群组的属性
+        // 包括 chainStyle & bias & weight
+
+    <android.support.constraint.Guideline
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:orientation="vertical"                      // 控制参考线的方向
+        app:layout_constraintGuide_begin="60dp"             // 参考线距离开始位置的绝对位置
+        app:layout_constraintGuide_end="60dp"               // 参考线距离结束位置的绝对位置
+        app:layout_constraintGuide_percent="0.66"/>         // 参考线相对位置
+
+</android.support.constraint.ConstraintLayout>
+
+ */
+
--- code/src/main/java/com/linsh/code/view/textview/LearnLayingOutTextView.java
+++ code/src/main/java/com/linsh/code/view/textview/LearnLayingOutTextView.java
@@ -1,7 +1,5 @@
 package com.linsh.code.view.textview;
 
-import com.linsh.code.R;
-
 /**
  * <pre>
  *    author : Senh Linsh
@@ -11,6 +9,39 @@ import com.linsh.code.R;
  * </pre>
  */
 public class LearnLayingOutTextView {
-
-    private int layout = R.layout.text_view;
 }
+/*
+
+<TextView
+    android:autoLink="web|email|phone"  // 是否识别为可点击的链接 (默认可点击, 可配合 linksClickable 使用)
+    android:drawableLeft="..."          // 左侧图片
+    android:drawablePadding="5dp"       // 图片和文字的距离
+    android:drawableStart="..."         // 开始处图片 (适配从右到左的阅读习惯语言)
+    android:ellipsize="end"             // 省略部分显示方式 [start:开始 end:结尾 middle:中间 marquee:跑马灯 none:不显示]
+    android:ems="10"                    // 宽度设置为 N 个字符的宽度 (测试约为汉字字符的宽度) (指定宽度后该项会失效)
+    android:includeFontPadding="false"  // 是否包含顶部和底部额外空白 (默认为 true)
+    android:lineSpacingExtra="4dp"      // 行间距
+    android:lineSpacingMultiplier="1.1" // 行间距倍数(下一行顶部到上一行顶部的距离占单行高度的倍数)
+                                        // 默认为1, 为 0 时上下行重合 (大于 1 增加行间距, 小于 1 减小行间距)
+                                        // lineSpacingExtra 和 lineSpacingMultiplier 可以同事使用, 两者互不影响
+    android:lines="2"                   // 文本行数 (设置两行就显示两行，即使第二行没有数据)
+    android:linksClickable="true"       // 链接是否可点击 (配合 autoLink 属性使用)
+    android:marqueeRepeatLimit="3"      // 在 ellipsize 为 marquee 模式下, 重复滚动的次数 [marquee_forever: 无限重复]
+    android:maxLength="30"              // 最大文本长度，超出部分不显示也不计算
+    android:maxLines="3"                // 最大显示行数 (存在 lines 时这个属性失效)
+    android:minLines="1"                // 最小显示行数
+    android:scrollHorizontally="true"   // marquee 模式下, 是否出现横拉条 (测试时并没有出现...)
+    android:shadowColor="#00F"          // 文字阴影颜色 (需要配合 shadowRadius 使用)
+    android:shadowDx="-4"               // 文字阴影横向坐标偏移
+    android:shadowDy="4"                // 文字阴影纵向坐标偏移
+    android:shadowRadius="10"           // 文字阴影半径
+    android:textAlignment="center"      // 文字对齐 (默认 center 居中对齐)
+    android:textAllCaps="false"         // 是否全部大写 (true 时小写英文字母自动变大写)
+    android:textAppearance="..."        // 字体外观
+    android:textColor="#F00"            // 字体颜色
+    android:textColorLink="#0F0"        // 文字链接的颜色
+    android:textSize="20sp"             // 字体大小
+    android:textStyle="normal"          // 字体样式 [normal:正常, bold:粗体, italic:斜体]
+    />
+
+*/
